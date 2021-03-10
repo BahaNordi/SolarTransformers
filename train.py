e07328e5b228fa3f0135b99c3d6fb07c4c9bbc92
AlexEhtNordi <ali.ehteshami.bejnordi@est.fib.upc.edu>
2021-03-10 17:29:05 +0100

    refactor

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ from torch.utils.tensorboard import SummaryWriter
 from apex import amp
 from apex.parallel import DistributedDataParallel as DDP
 
-from models.modeling import VisionTransformer, CONFIGS
+from SolarTransformers.models.modeling import VisionTransformer, CONFIGS
 from utils.scheduler import WarmupLinearSchedule, WarmupCosineSchedule
 from utils.data_utils import get_loader
 from utils.dist_util import get_world_size
