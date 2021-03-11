ad257d58aa5cdd35fbb554585329b693f1814e84
AlexEhtNordi <ali.ehteshami.bejnordi@est.fib.upc.edu>
2021-03-11 13:38:24 +0100

    change scale value

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -19,9 +19,8 @@ def get_loader(args):
         torch.distributed.barrier()
 
     transform_train = transforms.Compose([
-        transforms.RandomApply(torch.nn.ModuleList([transforms.RandomCrop((40, 24), padding=(2, 2, 0, 0))]),
-                               p=0.5),
-        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
+
+        transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.7, 1.0)),
         transforms.RandomApply(torch.nn.ModuleList([transforms.ColorJitter(brightness=0.05, contrast=0.05,
                                                                            saturation=0.1, hue=0.05)]), p=0.3),
         transforms.RandomApply(torch.nn.ModuleList([transforms.GaussianBlur([3], sigma=(0.1, 2.0))]), p=0.3),
