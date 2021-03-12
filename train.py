e7e8819c5c4d96ef26f4481327263bfd2a3f861c
AlexEhtNordi <ali.ehteshami.bejnordi@est.fib.upc.edu>
2021-03-12 17:03:54 +0100

    train on cifar100

--- train.py
+++ train.py
@@ -288,9 +288,9 @@ def main():
                         help="Resolution size")
     parser.add_argument("--train_batch_size", default=32, type=int,
                         help="Total batch size for training.")
-    parser.add_argument("--eval_batch_size", default=64, type=int,
+    parser.add_argument("--eval_batch_size", default=128, type=int,
                         help="Total batch size for eval.")
-    parser.add_argument("--eval_every", default=5, type=int,
+    parser.add_argument("--eval_every", default=120, type=int,
                         help="Run prediction on validation set every so many steps."
                              "Will always run one evaluation at the end of training.")
 
